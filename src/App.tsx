67b1b9c6436c834e438fa106248112da64617daf
Weiwei <w1216lin@gmail.com>
2023-03-03 17:22:30 -0600

    feat: add category scroller

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,19 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Checkout from "./pages/Checkout";
+import Home from "./pages/Home";
 import Success from "./pages/Success";
+import Test from "./pages/Test";
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/checkout" >
-          <Route index element={<Checkout />}/>
-          <Route path="success" element={<Success />}/>
+        <Route path="/checkout">
+          <Route index element={<Checkout />} />
+          <Route path="success" element={<Success />} />
         </Route>
+        <Route path="/test" element={<Test />} />
       </Routes>
     </Router>
   );
